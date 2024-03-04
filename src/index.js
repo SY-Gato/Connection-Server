b24b25aa1bcf9380d63dd617c20089bf4043eec6
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-04 10:11:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-
+const WebSocket = require('ws');
+var wss = new WebSocket.Server({ port: 8080 });
+console.log("Running On Port 8080");
+wss.on('connection', function connection(ws) {
+  console.log("New Connection");
+  ws.on('message', function incoming(message) {
+    console.log(message);
+    ws.send(message);
+  });
+});
