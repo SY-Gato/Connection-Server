6bede1c0930355b3266a01628efa4fc70a64265c
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-04 10:56:32 -0600

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const WebSocket = require('ws');
+/*const WebSocket = require('ws');
 var wss = new WebSocket.Server({ port: 8080 });
 console.log("Running On Port 8080");
 wss.on('connection', function connection(ws) {
@@ -6,5 +6,27 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log(message);
     ws.send(message);
+  });
+});*/
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+server.listen(8080);
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+  console.log("New Connection");
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(process.memoryUsage()), 
+      function() { /* ignore errors */ });
+  }, 100);
+  ws.on('close', function() {
+    console.log("Connection Closed");
+    clearInterval(id);
   });
 });
