e86c44bf33b159208fd7efef3661fa47c6a7ad83
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-11 10:22:57 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ wss.on('connection', function connection(ws) {
     ws.send(message);
   });
 });*/
-exports.handler = function(event, context) {
+/*exports.handler = function(event, context) {
 var WebSocketServer = require('ws').Server
   , http = require('http');
 
@@ -20,11 +20,11 @@ wss.on('connection', function(ws) {
   console.log("New Connection");
   var id = setInterval(function() {
     ws.send(JSON.stringify(process.memoryUsage()), 
-      function() { /* ignore errors */ });
-  }, 100);
+      function() { /* ignore errors */// });
+  /*}, 100);
   ws.on('close', function() {
     console.log("Connection Closed");
     clearInterval(id);
   });
 });
-};
+};*/
